3680d5e57ef9f995c0fb15c6908876d86650d534
Mahmoud Bentriou <moud@MacBook-Pro-de-Mahmoud.local>
2023-03-01 09:07:35 +0100

    Add the blank background to the processed image

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 def process_image(input_image_path, output_image_path = "", save_image = False):
     image_removed_bg = remove_background(input_image_path)
-    image_output = image_removed_bg
+    image_output = blank_background(image_removed_bg)
 
     if save_image:
         assert os.path.isdir(os.path.dirname(output_image_path))
@@ -21,4 +21,10 @@ def remove_background(input_image_path):
     image_output = rembg.remove(image_input)
 
     return image_output
+
+def blank_background(image):
+    blank_bg_image = Image.new("RGBA", image.size, "WHITE")
+    blank_bg_image.paste(image, (0, 0), image)
+
+    return blank_bg_image
 
